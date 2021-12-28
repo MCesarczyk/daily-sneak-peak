3562be481545f4a9712cf0f6c8eddca43080d974
MCesarczyk <michal.cesarczyk@gmail.com>
2021-12-28 21:04:42 +0100

    disconnect original controller

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,70 +1,73 @@
 class ChildrenController < ApplicationController
-  before_action :set_child, only: %i[ show edit update destroy ]
-
-  # GET /children or /children.json
   def index
-    @children = Child.all
   end
+  
+  # before_action :set_child, only: %i[ show edit update destroy ]
 
-  # GET /children/1 or /children/1.json
-  def show
-  end
+  # # GET /children or /children.json
+  # def index
+  #   @children = Child.all
+  # end
 
-  # GET /children/new
-  def new
-    @child = Child.new
-  end
+  # # GET /children/1 or /children/1.json
+  # def show
+  # end
 
-  # GET /children/1/edit
-  def edit
-  end
+  # # GET /children/new
+  # def new
+  #   @child = Child.new
+  # end
 
-  # POST /children or /children.json
-  def create
-    @child = Child.new(child_params)
+  # # GET /children/1/edit
+  # def edit
+  # end
 
-    respond_to do |format|
-      if @child.save
-        format.html { redirect_to child_url(@child), notice: "Child was successfully created." }
-        format.json { render :show, status: :created, location: @child }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @child.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # POST /children or /children.json
+  # def create
+  #   @child = Child.new(child_params)
 
-  # PATCH/PUT /children/1 or /children/1.json
-  def update
-    respond_to do |format|
-      if @child.update(child_params)
-        format.html { redirect_to child_url(@child), notice: "Child was successfully updated." }
-        format.json { render :show, status: :ok, location: @child }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @child.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @child.save
+  #       format.html { redirect_to child_url(@child), notice: "Child was successfully created." }
+  #       format.json { render :show, status: :created, location: @child }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @child.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /children/1 or /children/1.json
-  def destroy
-    @child.destroy
+  # # PATCH/PUT /children/1 or /children/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @child.update(child_params)
+  #       format.html { redirect_to child_url(@child), notice: "Child was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @child }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @child.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-    respond_to do |format|
-      format.html { redirect_to children_url, notice: "Child was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /children/1 or /children/1.json
+  # def destroy
+  #   @child.destroy
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_child
-      @child = Child.find(params[:id])
-    end
+  #   respond_to do |format|
+  #     format.html { redirect_to children_url, notice: "Child was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-    # Only allow a list of trusted parameters through.
-    def child_params
-      params.require(:child).permit(:name, :surname, :breakfast, :souptime, :sleep, :secondcourse, :snack, :supplies, :comment, :dates)
-    end
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_child
+  #     @child = Child.find(params[:id])
+  #   end
+
+  #   # Only allow a list of trusted parameters through.
+  #   def child_params
+  #     params.require(:child).permit(:name, :surname, :breakfast, :souptime, :sleep, :secondcourse, :snack, :supplies, :comment, :dates)
+  #   end
 end
