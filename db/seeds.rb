9ac104bf437146cfd62d3637056c801035829c61
MCesarczyk <michal.cesarczyk@gmail.com>
2021-12-28 15:10:40 +0100

    add mockup data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Child.create(name:'Jaś', surname:'Kowalski', breakfast:'half', souptime:'all', sleep:'none', secondcourse:'all', snack:'all', supplies:'towel', comment:'broken tooth', dates:'meeting 12.12.2022')
+Child.create(name:'Małgosia', surname:'Nowak', breakfast:'all', souptime:'none', sleep:'1 hour', secondcourse:'half', snack:'none', supplies:'basket', comment:'beaten up wolf', dates:'meeting 12.12.2022')
